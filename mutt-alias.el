738f36a40b4e5698c5e92180fdc5dabd32bfdb71
Dave Pearson <davep@davep.org>
2018-08-20 09:09:03 +0100

    :bookmark: v1.5
    For #2.

--- mutt-alias.el
+++ mutt-alias.el
@@ -2,7 +2,7 @@
 ;; Copyright 1999-2018 by Dave Pearson <davep@davep.org>
 
 ;; Author: Dave Pearson <davep@davep.org>
-;; Version: 1.4
+;; Version: 1.5
 ;; Keywords: mail, mutt
 ;; URL: https://github.com/davep/mutt-alias.el
 
